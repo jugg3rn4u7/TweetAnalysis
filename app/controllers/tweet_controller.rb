43ed27445a123f6c15e315648669bda0ced1da72
jugg3rn4u7 <shriku4u27@gmail.com>
2016-05-10 02:38:15 -0700

    10-fold Cross Validation

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,5 +1,6 @@
 require 'sad_panda'
 require 'madeleine'
+include ActionView::Helpers::NumberHelper
 class TweetController < ApplicationController
   skip_before_action :verify_authenticity_token
   @@m = SnapshotMadeleine.new("bayes_data") {
@@ -105,16 +106,21 @@ class TweetController < ApplicationController
     end
   end
   def train_classifier
-    begin    
-      @tweets = Tweet.all
+    begin
+      puts "Training Classifier Started !"
+      Predict.delete_all 
+      data = Tweet.all 
+      data.each do |record|
+        Predict.create(id: record.id, screen_name: record.screen_name, text: record.text, actual: record.political_affiliation)
+      end  
+      fold = cross_validation()
+      @tweets = Predict.all
       @tweets.each do |tweet|
-        if tweet.political_affiliation == 1
+        if tweet["predicted#{fold}"] == 1
            @@m.system.train_democratic tweet.text
-        elsif tweet.political_affiliation == 2
+        elsif tweet["predicted#{fold}"] == 2
            @@m.system.train_republican tweet.text
         end
-        tweet.attributes = { :mode => 1 }
-        tweet.save
       end  
       @@m.take_snapshot
       puts "Training Classifier Complete !"
@@ -152,7 +158,7 @@ class TweetController < ApplicationController
   end
   def getDemocratsList
     begin
-      @democrats = Tweet.select( "screen_name" ).where( political_affiliation: 1 ) 
+      @democrats = Tweet.select( "screen_name" ).where( political_affiliation: 1 ).uniq
       puts "Get Democrats List Complete !"
       render :json => @democrats.to_json, :status => 200 
     rescue Exception => e
@@ -162,7 +168,7 @@ class TweetController < ApplicationController
   end
   def getRepublicanList
     begin
-      @republicans = Tweet.select( "screen_name" ).where( political_affiliation: 2 )
+      @republicans = Tweet.select( "screen_name" ).where( political_affiliation: 2 ).uniq
       puts "Get Republicans List Complete !"
       render :json => @republicans.to_json, :status => 200 
     rescue Exception => e
@@ -179,5 +185,95 @@ class TweetController < ApplicationController
       puts e.message
       render :nothing => true, :status => 500
     end
+  end
+  def cross_validation
+    puts "Cross Validation Started !"
+    client = Mysql2::Client.new(:host => "localhost", :database => "TweetAnalysis_development", :username => "root", :password => "root")
+    result = client.query("select count(*) - count(*)/10 as Training, count(*)/10 as Testing from cross_validation;")
+    training = 0
+    testing = 0
+    result.each do |row|
+     training = row["Training"].to_f
+     testing = row["Testing"].to_f
+    end
+    puts "Training Records Count : #{training} , Testing Records Count : #{testing}"
+    accuracy = []  
+    fold = 10
+    i = 1
+    while i <= fold do
+      puts "Cross Validation: Fold #{i} started"
+      _start = (i - 1) * testing
+      _end = i * testing - 1
+      setFold( _start, _end )
+      cross_validate(i)
+      accuracy << calculate_accuracy(i)
+      puts "Cross Validation: Fold #{i} complete."
+      i += 1
+    end 
+    max_accuracy = accuracy.max
+    max_accuracy_fold = accuracy.index(accuracy.max) + 1
+    puts "Accuracies #{accuracy}"
+    puts "Maximum Accuracy at: Fold #{max_accuracy_fold} with Accuracy #{max_accuracy}"
+    puts "Cross Validation Complete !"
+    return max_accuracy_fold
+  end
+  def cross_validate(fold)
+    begin 
+      client = Mysql2::Client.new(:host => "localhost", :database => "TweetAnalysis_development", :username => "root", :password => "root")
+      predicted_field = "predicted#{fold}"
+      #Train 9/10 folds - classifier   
+      classifier = Classifier::Bayes.new 'Democratic', 'Republican'
+      table = client.query("select * from cross_validation where mode = '1';")
+      table.each do |record|
+        if record["actual"].to_i == 1
+           classifier.train_democratic record["text"]
+        elsif record["actual"].to_i == 2
+           classifier.train_republican record["text"]
+        end
+      end 
+
+      #Test 1/10 fold - classifier
+      test = client.query("select * from cross_validation where mode = '2';")
+      t_party = ""
+      party = 0
+      _id = 0
+      test.each do |record|
+        t_party = classifier.classify record["text"]
+        if t_party == "Democratic"
+          party = 1
+        elsif t_party == "Republican"
+          party = 2
+        end
+        _id = record["id"]
+        client.query("update cross_validation set #{predicted_field} = '#{party}' where id = '#{_id}';")
+      end   
+    rescue Exception => e
+      puts e.message
+    end
+  end
+  def setFold(_start, _end)
+    i = 0
+    table = Predict.all
+    table.each do |record|
+      if i >= _start && i <= _end
+        record.attributes = { :mode => 2 }
+      else     
+        record.attributes = { :mode => 1 }
+      end
+      record.save
+      i += 1
+    end  
+  end
+  def calculate_accuracy(fold)
+    table = Predict.all
+    record_count = table.length
+    accuracy_count = 0
+    table.each do |record|
+      if record["actual"].to_i == record["predicted#{fold}"].to_i
+        accuracy_count += 1
+      end
+    end
+    accuracy = ( accuracy_count.to_f / record_count ) * 100
+    return number_with_precision( accuracy , precision: 2)
   end
 end
---
+++ app/models/predict.rb
@@ -0,0 +1,3 @@
+class Predict < ActiveRecord::Base
+	self.table_name = "cross_validation"
+end
